6fa61b467fbb725e0d09745c45181678760a5aba
nj241098 <nisha.jain@grayopus.com>
2021-07-08 14:10:20 +0530

    Sign In and Sign Up page Ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chit_chat/views/sign_in.dart';
+import 'package:flutter_chit_chat/views/sign_up.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,11 +14,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
+        scaffoldBackgroundColor: Colors.blue.shade100,
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(title: Text("ChitChat"),),
-      ),
+      home: SignUpPage(),
     );
   }
 }
